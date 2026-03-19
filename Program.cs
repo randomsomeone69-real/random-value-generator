18580515f97200761fb0cfad994c8ba7e238b390
randomsomeone69 <randomsomeone69@outlook.com>
2026-03-19 11:14:39 +0100

    Added different types of tape play speed.

--- Program.cs
+++ Program.cs
@@ -5,4 +5,12 @@
     return something[alea.Next(5)]; //génére un nombre aléatoire, puis va chercher la valeur de la génération
 }
 
+string Duration()
+{
+    string[] something = new string[] { "SP", "LP", "EP/SLP", "VP (NTSC)" };
+    Random alea = new Random(); // commande de génération de nombre aléatoire
+    return something[alea.Next(4)]; //génére un nombre aléatoire, puis va chercher la valeur de la génération
+}
+
 Console.WriteLine(ReturnSomething());
+Console.WriteLine(Duration());
