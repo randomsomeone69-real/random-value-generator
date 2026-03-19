d7f6e7f5ef7c71f738bab78a32dd574d5656f575
randomsomeone69 <randomsomeone69@outlook.com>
2026-03-19 11:00:48 +0100

    Added program.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string ReturnSomething()
+{
+    string[] something = new string[] { "VHS", "S-VHS", "VHS-C", "D-VHS", "Betamax" };
+    Random alea = new Random(); // commande de génération de nombre aléatoire
+    return something[alea.Next(5)]; //génére un nombre aléatoire, puis va chercher la valeur de la génération
+}
+
+Console.WriteLine(ReturnSomething());
