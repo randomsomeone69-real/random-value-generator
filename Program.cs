32e6f32b7b118ac03210cb252452b93a2e9cbe9c
randomsomeone69 <randomsomeone69@outlook.com>
2026-03-24 10:54:44 +0100

    Added Data type (USSR compliant)

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@
 
 string Duration()
 {
-    string[] something = new string[] { "SP", "LP", "EP/SLP", "VP (NTSC)" };
+    string[] something = new string[] { "SP", "LP", "EP/SLP", "VP (NTSC)", "Data (USSR)" };
     Random alea = new Random(); // commande de génération de nombre aléatoire
-    return something[alea.Next(4)]; //génére un nombre aléatoire, puis va chercher la valeur de la génération
+    return something[alea.Next(something.Length)]; //génére un nombre aléatoire basé sur la longueur du tableau, puis va chercher la valeur de la génération
 }
 
 void ShowDescriptor()
